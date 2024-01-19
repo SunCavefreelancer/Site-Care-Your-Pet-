184b9deb85b4d75c51ced6564083d4c0b88307b5
Code_GG <139456030+SunCavefreelancer@users.noreply.github.com>
2024-01-19 20:43:36 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 // Строгий режим
 "use strict"
 
+
+const icon = document.querySelector('.icon-menu');
+icon.addEventListener('click', function () {
+	document.documentElement.classList.toggle('menu-open');
+});
+
+/* ---------------------------------БУРГЕР МЕНЮ---------------------------------------- */
+
 /* =====================================sliderFON=============================================== */
 
 
